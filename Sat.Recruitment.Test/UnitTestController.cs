16b2261fd4eb80a6e448b1cc5873a77052686944
Katia Perchet <katiamperchet@gmail.com>
2023-05-21 21:44:20 -0300

    Unit testing for Controller file

---
+++ Sat.Recruitment.Test/UnitTestController.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Dynamic;
+
+using Microsoft.AspNetCore.Mvc;
+
+using Presentation.Controllers;
+
+using Xunit;
+
+namespace Sat.Recruitment.Test
+{
+    [CollectionDefinition("Tests", DisableParallelization = true)]
+    public class UnitTestController
+    {
+        [Fact]
+        public void CreateUser_SuccesfulResult()
+        {
+           //Arrange
+           var userController = new UsersController();
+           //Act
+           var result = userController.CreateUser("Mike", "mike@gmail.com", "Av. Juan G", "+349 1122354215", "Normal", "124");
+           //Assert
+           Assert.True(result.IsSuccess);
+           Assert.Equal("User Created", result.Errors);
+        }
+
+        [Fact]
+        public void CreateUser_DuplicatedField_FailedResult()
+        {
+            //Arrange
+            var userController = new UsersController();
+            //Act
+            var result = userController.CreateUser("Agustina", "Agustina@gmail.com", "Av. Juan G", "+349 1122354215", "Normal", "124");
+            //Assert
+            Assert.False(result.IsSuccess);
+            Assert.Equal("The user is duplicated", result.Errors);
+        }
+
+        [Fact]
+        public void CreateUser_BlankField_FailedResult()
+        {
+            //Arrange
+            var userController = new UsersController();
+            //Act
+            var result = userController.CreateUser("", "", "", "", "", "");
+            //Assert
+            Assert.False(result.IsSuccess);
+            Assert.NotEmpty(result.Errors);
+        }
+    }
+}
