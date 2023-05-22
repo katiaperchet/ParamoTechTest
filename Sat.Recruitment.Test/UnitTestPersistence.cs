0ccdcd7393e31a4f309f17c21b6315b775dc8146
Katia Perchet <katiamperchet@gmail.com>
2023-05-21 21:47:57 -0300

    Created unit testing for Persistence

---
+++ Sat.Recruitment.Test/UnitTestPersistence.cs
@@ -0,0 +1,39 @@
+﻿using Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Sat.Recruitment.Test
+{
+	public class UnitTestPersistence
+	{
+        [Fact]
+        public void ReadUsersFromFile_ReturnsStreamReader()
+        {
+            // Arrange
+            var expectedPath = Path.Combine(Directory.GetCurrentDirectory(), "Files/Users.txt");
+            // Act
+            var result = UserPersistence.ReadUsersFromFile();
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<StreamReader>(result);
+        }
+        
+        [Fact]
+        public void SaveUserToFile_ValidUser_Sucess()
+        {
+            // Arrange
+            var user = "Juan,Juan@gmail.com,+549115412,Paraguay 2464,Normal,1234";
+            // Act
+            var result = UserPersistence.SaveUserToFile(user);
+            // Assert
+            Assert.True(result);
+        }
+
+
+    }
+}
